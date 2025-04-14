7663f13149b5e296dab143f673b8511d7fe88bf2
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-04-14 15:59:40 +0900

    プロフィールの編集の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,32 @@ class UsersController < ApplicationController
   def profile
     @user = User.find_by(params[:user_name])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.user_avatar.attached?
+      @user.user_avatar.attach(params[:user][:user_avatar])
+    else
+      @user.user_avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-user-avatar.png')),
+      filename: 'default-user-avatar.png',
+      content_type: 'image/png')
+    end
+
+    if @user.update(user_params)
+      flash[:notice] = "ユーザープロフィールを更新しました。"
+      redirect_to :users_profile, status: :unprocessable_entity
+    else
+      render "edit"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:user_name, :user_avatar, :user_introduction)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/account'
   get 'users/profile'
-  devise_for :users
   get 'homes/top'
+  devise_for :users
+  resources :users
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
