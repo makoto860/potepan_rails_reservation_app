7113dd9f4330ee6c92b8b41c823b1b2c70a09a27
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-05-05 15:08:50 +0900

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  # root :toは先頭にする
   get 'rooms/own'
   get 'users/account'
   get 'users/profile'
@@ -9,6 +11,9 @@ Rails.application.routes.draw do
   get 'homes/sapporo'
   get 'homes/area_search'
 
+  #get 'homes/area/:name', to: 'homes#area', as: 'area_home'
+  #これで /homes/area/tokyo や /homes/area/kyoto のようにアクセス可能です。params[:name] でエリア名をコントローラーに渡せるかと思いました
+  
   devise_for :users
   resources :users
 
@@ -23,6 +28,5 @@ Rails.application.routes.draw do
       post 'confirmation', to: 'reservations#confirmation'
     end
   end
-  root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
